2ced3d83eec7d600c1c55103e7977198bd1370b0
jos3lo89 <ganbaru743@gmail.com>
2025-08-10 22:52:56 -0500

    pathname

--- app/products/[productId]/reviews/[reviewId]/not-found.tsx
+++ app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -1,7 +1,16 @@
+"use client";
+import { usePathname } from "next/navigation";
+
 const NotFoundPage = () => {
+  const pathname = usePathname();
+
+  const productId = pathname.split("/")[2];
+  const reviewId = pathname.split("/")[4];
   return (
     <div>
-      <h2>revision no encontrada</h2>
+      <h2>
+        revision {reviewId} no encontrada del producto {productId}
+      </h2>
     </div>
   );
 };
